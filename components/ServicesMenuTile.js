76685f92d6420db125723b4105972a3ba528b0ea
faisal165 <58952592+faisal165@users.noreply.github.com>
2022-12-29 20:55:24 +0500

    services page created

--- components/ServicesMenuTile.js
+++ components/ServicesMenuTile.js
@@ -5,7 +5,7 @@ import { ServicesElements } from './ServicesElements'
 export default function ServicesMenuTile({title,iconUrl}) {
   return (
     <div className=' flex flex-col w-24 justify-center place-items-center'>
-        <div className="p-4 bg-gradient-to-b from-white  to-gray-100 rounded-md flex  w-20 h-20 justify-center place-items-center">
+        <div className="p-4 bg-gradient-to-b from-white  to-gray-100 rounded-2xl flex  w-20 h-20 justify-center place-items-center">
         <div className='flex w-10 h-10 object-contain place-items-center '>
         <img
         src={iconUrl}></img>
--- pages/Services/index.js
+++ pages/Services/index.js
@@ -10,7 +10,7 @@ export default function index() {
       <SideBar></SideBar>
       <div className=" flex flex-col w-3/4 space-y-4 px-8">
         <Header></Header>
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-4">
           <p className="text-lg font-semibold">Services</p>
           <div className="p-8 bg-slate-200 w-full h-84 rounded-3xl">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 justify-center place-items-center">
@@ -23,6 +23,16 @@ export default function index() {
             </div>
             
 
+          </div>
+          <div className="flex  rounded-3xl w-full h-40  bg-slate-200 justify-between place-items-center p-8">
+            <div className="flex flex-col  ">
+              <p className=" font-semibold text-lg ">Shift Details</p>
+              <p className="text-sm pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br/>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+            </div>
+              <div className=" flex w-40 h-30 object-contain">
+              <img src="images/ShiftDetailIllustration.png">
+              </img>
+              </div>
           </div>
 
         </div>
